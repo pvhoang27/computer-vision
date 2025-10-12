fe48d5923866fef90dffdb67fd05483ebe545049
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-10-12 17:14:24 +0700

    update

--- ex.py
+++ ex.py
@@ -1,22 +1,22 @@
 import cv2 
 
 image = cv2.imread("001771721_jpg.rf.687025a63ae5c9e58f2454ab1e41eaa9.jpg")
-
 height ,width , _ = image.shape
 
 with open("001771721_jpg.rf.687025a63ae5c9e58f2454ab1e41eaa9.txt" , "r") as to :
   annotation = to.readlines()
-
-# print(annotation)
-
 annotation = [anno.rstrip().split(" ") for anno  in  annotation]
-
-# print(annotation)
 for  anno in annotation: 
   category, x , y , w , h = anno 
-  x = float(x)
-  y = float(y)
-  w = float(w)
-  h = float(h)
-  cv2.rectangle(image, (5, 5), (220, 220), (255, 0, 0), 2)
+  xcent= float(x) * width
+  ycent = float(y) * height
+  w = float(w)* width
+  h = float(h)* height
+  xtl = int(xcent - w /2 )
+  ytl = int(ycent - h / 2)
+  xbr = int(xcent + w / 2) 
+  ybr = int(ycent + h / 2)
+  cv2.rectangle(image, (xtl, ytl), (xbr , ybr), (0, 255, 0), 2)
+
+cv2.imwrite("sample.jpg", image)
 
